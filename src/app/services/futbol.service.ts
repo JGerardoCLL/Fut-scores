a4a5d84ebf3a0457f91020517247a03ecc86c7e8
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-23 01:06:13 -0600

    importar modelo y usar la estructura para la respuesta de la api

--- src/app/services/futbol.service.ts
+++ src/app/services/futbol.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable,inject } from "@angular/core";
 import { Observable } from "rxjs";
+import { Team, SportmonksResponse } from "../models/futbol.model";
+
 
 @Injectable({
     providedIn: 'root'
@@ -10,8 +12,9 @@ export class FutbolService{
     private readonly http = inject(HttpClient);
     private readonly apiUrl = '/api/v3/football/teams';
 
-    getTeams(): Observable<any>{
-        return this.http.get(this.apiUrl, {
+    getTeams(): Observable<SportmonksResponse<Team>>{
+
+        return this.http.get<SportmonksResponse<Team>>(this.apiUrl, {
             params: {
                 api_token: 'pdUyAj1tVHoiYHfsrlCxra6lWAHx8HlDqA1uiFqaqGtBiTbGIhu2MT2FWZUc'
             }
