babd63395e9a6d89531321d03e6daf768622610b
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-22 21:01:11 -0600

    inyectar serviciomen  el componente y solicitar y mostrar equipos

--- src/app/components/pantalla principal/principal.ts
+++ src/app/components/pantalla principal/principal.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { FutbolService  } from '../../services/futbol.service';
 
 @Component({
     selector: 'app-principal',
@@ -9,6 +10,22 @@ import {CommonModule} from '@angular/common';
     styleUrl: './principal.css'
 })
 
-export class PrincipalComponent{
+export class PrincipalComponent implements OnInit{
+
+    private readonly futbolService = inject(FutbolService);
+
+    teams = signal<any[]>([]);
+
+    ngOnInit(): void {
+        this.futbolService.getTeams().subscribe({
+            next: (response) => {
+                console.log('Primer equipo:',response.data[0]);
+                this.teams.set(response.data);
+            },
+            error: (error) => {
+                console.error('Error al obtener los equipos:', error);
+            }
+        });
+    }
     
 }
