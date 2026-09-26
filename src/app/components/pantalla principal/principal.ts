0fb24640ffd9a318725507b5b7d86f382d928fb9
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-25 22:53:01 -0600

    cambio en componente prueba

--- src/app/components/pantalla principal/principal.ts
+++ src/app/components/pantalla principal/principal.ts
@@ -1,7 +1,8 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FutbolService } from '../../services/futbol.service';
-import { Team } from '../../models/futbol.model';
+import { Team, Match } from '../../models/futbol.model';
+import { finalize } from 'rxjs';
 
 @Component({
     selector: 'app-principal',
@@ -16,22 +17,28 @@ export class PrincipalComponent implements OnInit{
     private readonly futbolService = inject(FutbolService);
 
     teams = signal<Team[]>([]);
+    matchs = signal<Match[]>([]);
     isLoading = signal<boolean>(false);
     errorMessage = signal<string | null>(null);
 
     ngOnInit(): void {
         this.isLoading.set(true);
         
-        this.futbolService.getTeams().subscribe({
+        this.futbolService.getTeams()
+        .pipe(
+          finalize(() => this.isLoading.set(false))
+        )
+        .subscribe({
             next: (response) => {
-                this.teams.set(response.data);
-                this.isLoading.set(false);
+            this.teams.set(response.data);
             },
             error: (error) => {
-                this.errorMessage.set('No se pudieron cargar los equipos.');
-                this.isLoading.set(false);
+            console.error('Error al obtener los equipos:', error);
+            this.errorMessage.set('No se pudieron cargar los equipos.');
             }
         });
     }
     
 }
+    
+
